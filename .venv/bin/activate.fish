8222ce3f2078e307d4726a14ed7af471ef5cbf6d
puji29 <pujilaksono608@gmail.com>
2024-03-07 18:42:27 +0700

    [FIX] fix all feature

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nomi/Documents/myskill/Backend/flask_fundamental/final-project/.venv"
+set -gx VIRTUAL_ENV "/home/nomi/Documents/mydata/data/myskill/Backend/flask_fundamental/final-project/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
